09c76b9302ddee6c5b17d7e864d491a33f0b83ea
fliu <bob@bob.bob>
2015-03-18 12:16:48 -0400

    Added stringCompressor

---
+++ src/test/StringCompressorTest.java
@@ -0,0 +1,34 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import puzzles.StringCompressor;
+
+public class StringCompressorTest {
+	@Rule
+	public ExpectedException ex = ExpectedException.none();
+
+	@Test
+	public void happy() throws Exception {
+		StringCompressor s = new StringCompressor("ffrreedd");
+		assertTrue(s.getAnswer().equals("f2r2e2d2"));
+	}
+
+	@Test
+	public void spaces() throws Exception {
+		StringCompressor s = new StringCompressor("ffrre e  dd");
+		assertTrue(s.getAnswer().equals("f2r2e e 2d2"));
+	}
+
+	@Test
+	public void nullInput() throws Exception {
+		ex.expect(Exception.class);
+		StringCompressor s = new StringCompressor(null);
+	}
+
+}
