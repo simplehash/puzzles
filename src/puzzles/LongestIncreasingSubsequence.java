d50e222a7f6a449b6357d0c6b2c95ef70efaf4aa
fliu <bob@bob.bob>
2015-03-26 15:32:36 -0400

    added

--- src/puzzles/LongestIncreasingSubsequence.java
+++ src/puzzles/LongestIncreasingSubsequence.java
@@ -6,53 +6,41 @@ public class LongestIncreasingSubsequence {
 	private int[] sequence;
 
 	public LongestIncreasingSubsequence(int[] numbers) {
-		sequence = numbers;
+		if (numbers != null && numbers.length > 0) {
+			sequence = numbers;
+		}
 	}
 
 	public int[] calculate() {
+		if (sequence == null || sequence.length < 1) {
+			return null;
+		}
 		List<Integer> tempLISIndex = new ArrayList<>();
-		List<Integer> tempLISValue = new ArrayList<>();
 		tempLISIndex.add(null);
-		tempLISValue.add(null);
 		tempLISIndex.add(0);
-		tempLISValue.add(sequence[0]);
 		Integer[] parentIndex = new Integer[sequence.length];
 		for (int i = 1; i < sequence.length; i++) {
 			int currentElement = sequence[i];
-			if (tempLISValue.get(tempLISValue.size() - 1) == null
-					|| currentElement > tempLISValue.get(tempLISValue.size() - 1)) {
-				tempLISValue.add(currentElement);
+			if (currentElement > sequence[tempLISIndex.get(tempLISIndex.size() - 1)]) {
 				tempLISIndex.add(i);
 				parentIndex[i] = tempLISIndex.get(tempLISIndex.size() - 2);
 			} else {
 				int j = 1;
-				for (; tempLISValue.get(j) < currentElement; j++)
+				for (; sequence[tempLISIndex.get(j)] < currentElement; j++)
 					;
 				tempLISIndex.remove(j);
 				tempLISIndex.add(j, i);
-				tempLISValue.remove(j);
-				tempLISValue.add(j, currentElement);
 				parentIndex[i] = tempLISIndex.get(j - 1);
 			}
 		}
-		for (Integer i : parentIndex) {
-			System.out.println(i);
 
-		}
 		int[] subsequence = new int[tempLISIndex.size() - 1];
-		subsequence[subsequence.length - 1] = tempLISValue.get(tempLISValue.size() - 1);
+		subsequence[subsequence.length - 1] = sequence[tempLISIndex.get(tempLISIndex.size() - 1)];
 		int currentIndex = tempLISIndex.get(tempLISIndex.size() - 1);
 		for (int i = subsequence.length - 2; i >= 0; i--) {
 			subsequence[i] = sequence[parentIndex[currentIndex]];
 			currentIndex = parentIndex[currentIndex];
 		}
-		print(subsequence);
 		return subsequence;
-	}
-
-	private void print(int[] subsequence) {
-		for (int i : subsequence) {
-			System.out.print(i + " ");
-		}
 	}
 }
--- src/test/LongestIncreasingSubsequenceTest.java
+++ src/test/LongestIncreasingSubsequenceTest.java
@@ -2,15 +2,38 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
 import puzzles.LongestIncreasingSubsequence;
 
 public class LongestIncreasingSubsequenceTest {
+	LongestIncreasingSubsequence l;
+
 	@Test
 	public void happy() {
-		LongestIncreasingSubsequence l = new LongestIncreasingSubsequence(
-				new int[] { 2, 3, 4, 5,1, 9 });
-		l.calculate();
+		l = new LongestIncreasingSubsequence(new int[] { 2, 3, 4, 5, 1, 9 });
+		assertTrue(Arrays.equals(l.calculate(), new int[] { 2, 3, 4, 5, 9 }));
+		l = new LongestIncreasingSubsequence(new int[] { 2, 6, 3, 4, 1, 2, 9, 5, 8 });
+		assertTrue(Arrays.equals(l.calculate(), new int[] { 2, 3, 4, 5, 8 }));
+	}
+
+	@Test
+	public void empty() {
+		l = new LongestIncreasingSubsequence(new int[] {});
+		assertNull(l.calculate());
+	}
+
+	@Test
+	public void nullNumbers() {
+		l = new LongestIncreasingSubsequence(null);
+		assertNull(l.calculate());
+	}
+
+	@Test
+	public void allSame() {
+		l = new LongestIncreasingSubsequence(new int[] { 1, 1, 1 });
+		assertTrue(Arrays.equals(l.calculate(), new int[] { 1 }));
 	}
 }
