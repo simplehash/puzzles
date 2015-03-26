fb1b534308856bc7f33fc1e99cd6d579fd763bb1
fliu <bob@bob.bob>
2015-03-26 15:38:26 -0400

    asdasdf

--- src/puzzles/LongestIncreasingSubsequence.java
+++ src/puzzles/LongestIncreasingSubsequence.java
@@ -15,16 +15,22 @@ public class LongestIncreasingSubsequence {
 		if (sequence == null || sequence.length < 1) {
 			return null;
 		}
+		// LIS of length i ending at sequence[i]
 		List<Integer> tempLISIndex = new ArrayList<>();
 		tempLISIndex.add(null);
 		tempLISIndex.add(0);
+		// The parent index of each index of int[] sequence that forms the
+		// overall LIS
 		Integer[] parentIndex = new Integer[sequence.length];
 		for (int i = 1; i < sequence.length; i++) {
 			int currentElement = sequence[i];
+			// Increasing #, add it on to the LIS list (this element makes a
+			// longer LIS)
 			if (currentElement > sequence[tempLISIndex.get(tempLISIndex.size() - 1)]) {
 				tempLISIndex.add(i);
 				parentIndex[i] = tempLISIndex.get(tempLISIndex.size() - 2);
-			} else {
+			} else { // Lower/equal number, replace the smallest # in
+						// sequence[tempLISIndex] that is still larger than it
 				int j = 1;
 				for (; sequence[tempLISIndex.get(j)] < currentElement; j++)
 					;
@@ -34,6 +40,7 @@ public class LongestIncreasingSubsequence {
 			}
 		}
 
+		// Build the overall LIS backwards with parentIndex and tempLISIndex
 		int[] subsequence = new int[tempLISIndex.size() - 1];
 		subsequence[subsequence.length - 1] = sequence[tempLISIndex.get(tempLISIndex.size() - 1)];
 		int currentIndex = tempLISIndex.get(tempLISIndex.size() - 1);
