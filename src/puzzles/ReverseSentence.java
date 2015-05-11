b91a8ec376f374826646a016755515258f76f3d4
fliu <bob@bob.bob>
2015-05-11 12:50:24 -0400

    gh

--- src/puzzles/ReverseSentence.java
+++ src/puzzles/ReverseSentence.java
@@ -1,21 +1,20 @@
 package puzzles;
 
 public class ReverseSentence {
-	private String answer = null;
+	private StringBuilder answer = null;
 
 	public ReverseSentence(String sentence) throws Exception {
 		if (sentence == null) {
 			throw new Exception("Null sentence");
 		}
 		String[] words = sentence.split(" ");
-		answer = "";
+		answer = new StringBuilder();
 		for (int i = words.length - 1; i >= 0; i--) {
-			answer += words[i] + " ";
+			answer.append(words[i]).append(" ");
 		}
-		answer = answer.trim();
 	}
 
 	public String getAnswer() {
-		return answer;
+		return answer.toString().trim();
 	}
 }
