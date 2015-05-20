015967769bc6609e70317e6f8b0c223a206558c5
SimpleHash <simple@hash.lol>
2015-05-19 22:03:16 -0400

    ds

--- src/puzzles/LongestCommonSubsequence.java
+++ src/puzzles/LongestCommonSubsequence.java
@@ -3,56 +3,43 @@ package puzzles;
 public class LongestCommonSubsequence {
 	public static void diff(String s1, String s2) {
 		if (s1 != null && s2 != null && !s1.equals(s2)) {
-			String s = null;
 			StringBuilder ops = new StringBuilder();
-			if (s1.isEmpty()) {
-				for (int i = 0; i < s2.length(); i++) {
-					ops.append("+");
-				}
-				s = s2;
-			} else if (s2.isEmpty()) {
-				for (int i = 0; i < s1.length(); i++) {
-					ops.append("+");
-				}
-				s = s1;
-			} else {
-				final String lcs = find(s1, s2);
-				StringBuilder changes = new StringBuilder();
-				int pos1 = 0;
-				int pos2 = 0;
-				int posLCS = 0;
-				while (true) {
-					if (pos1 == s1.length()) {
-						changes.append(s2.substring(pos2));
-						for (int j = 0; j < (s2.length() - pos2); j++) {
-							ops.append("+");
-						}
-						break;
-					} else if (pos2 == s2.length()) {
-						changes.append(s1.substring(pos1));
-						for (int j = 0; j < (s1.length() - pos1); j++) {
-							ops.append("-");
-						}
-						break;
-					} else if (lcs.charAt(posLCS) == s1.charAt(pos1) && lcs.charAt(posLCS) == s2.charAt(pos2)) {
-						pos1++;
-						pos2++;
-						posLCS++;
-					} else if (s1.charAt(pos1) != lcs.charAt(posLCS)) {
-						changes.append(s1.charAt(pos1));
-						ops.append("-");
-						pos1++;
-					} else if (lcs.charAt(posLCS) != s2.charAt(pos2)) {
-						changes.append(s2.charAt(pos2));
+
+			final String lcs = find(s1, s2);
+			StringBuilder changes = new StringBuilder();
+			int pos1 = 0;
+			int pos2 = 0;
+			int posLCS = 0;
+			while (true) {
+				if (pos1 == s1.length()) {
+					changes.append(s2.substring(pos2));
+					for (int j = 0; j < (s2.length() - pos2); j++) {
 						ops.append("+");
-						pos2++;
-					} else {
-						System.err.println("wtf happened?");
 					}
+					break;
+				} else if (pos2 == s2.length()) {
+					changes.append(s1.substring(pos1));
+					for (int j = 0; j < (s1.length() - pos1); j++) {
+						ops.append("-");
+					}
+					break;
+				} else if (lcs.charAt(posLCS) == s1.charAt(pos1) && lcs.charAt(posLCS) == s2.charAt(pos2)) {
+					pos1++;
+					pos2++;
+					posLCS++;
+				} else if (s1.charAt(pos1) != lcs.charAt(posLCS)) {
+					changes.append(s1.charAt(pos1));
+					ops.append("-");
+					pos1++;
+				} else if (lcs.charAt(posLCS) != s2.charAt(pos2)) {
+					changes.append(s2.charAt(pos2));
+					ops.append("+");
+					pos2++;
+				} else {
+					System.err.println("wtf happened?");
 				}
-				s = changes.toString();
 			}
-			System.out.println(s);
+			System.out.println(changes.toString());
 			System.out.println(ops);
 		}
 	}
