730767405c4e2f2601aa7ba22951e2caff5959fd
fliu <bob@bob.bob>
2015-05-27 16:02:59 -0400

    asdf

--- src/simpleDB/Main.java
+++ src/simpleDB/Main.java
@@ -17,22 +17,6 @@ public class Main {
 		return transactions.size();
 	}
 
-	private static void unset(String name) {
-
-	}
-
-	private static void get(String name) {
-
-	}
-
-	private static void numequalto(String name) {
-
-	}
-
-	private static void set(String name, String value) {
-
-	}
-
 	private static void begin() {
 
 	}
@@ -45,11 +29,7 @@ public class Main {
 
 	}
 
-	private static void runTransactionCommand(String name, String value) {
-
-	}
-
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		database = new HashMap<>();
 		transactions = new Stack<>();
 		valueCount = new HashMap<>();
@@ -58,25 +38,18 @@ public class Main {
 		String command = "";
 		Transaction t = new Transaction();
 		while (!command.equals("end")) {
-			try {
-				command = br.readLine();
-				String[] temp = command.split(" ");
-				temp[0] = temp[0].toLowerCase().trim();
-				if (temp[0].equals("set") || temp.length == 3) {
+			command = br.readLine();
+			String[] temp = command.split(" ");
+			temp[0] = temp[0].toLowerCase().trim();
+			if (temp[0].equals("set") || temp.length == 3) {
+				t.add(temp[0], temp[1], temp[2]);
+			} else if (temp[0].equals("get") || temp[0].equals("unset") || temp[0].equals("numequalto")
+					&& temp.length == 2) {
+				t.add(temp[0], temp[1]);
+			} else if (temp[0].equals("begin") || temp[0].equals("rollback") || temp[0].equals("commit")) {
 
-				} else if (temp[0].equals("get") || temp[0].equals("unset") || temp[0].equals("numequalto")
-						&& temp.length == 2) {
-					t.add(temp[0], temp[1]);
-				} else if (temp[0].equals("begin") || temp[0].equals("rollback") || temp[0].equals("commit")) {
-
-				}
-			} catch (IOException e) {
-				System.err.println("I/O error while reading command.");
-				return;
 			}
 		}
-		while (!t.isEmpty()) {
-
-		}
+		t.commit(database);
 	}
 }
--- src/simpleDB/Transaction.java
+++ src/simpleDB/Transaction.java
@@ -6,43 +6,86 @@ public class Transaction {
 	private Stack<String[]> commands;
 	private Map<String, String> currentState;
 	private Map<String, Integer> currentValueCount;
+	private Set<String> justDeleted;
 
 	public Transaction() {
 		commands = new Stack<>();
 		currentState = new HashMap<>();
 		currentValueCount = new HashMap<>();
+		justDeleted = new HashSet<>();
 	}
 
-	public String add(String name, String value) {
-		commands.push(new String[] { name, value });
-
+	public String add(String... args) {
+		String command = args[0];
+		String key = null;
+		String value = null;
 		int valueCount = 0;
-		if (currentValueCount.containsKey(value)) {
-			valueCount = currentValueCount.get(value);
+
+		if (command.equals("get") || command.equals("unset")) {
+			key = args[1];
+		} else if (command.equals("set")) {
+			key = args[1];
+			value = args[2];
+		} else if (command.equals("numequalto")) {
+			value = args[1];
+			if (currentValueCount.containsKey(value)) {
+				valueCount = currentValueCount.get(value);
+			}
 		}
 
-		if (name.equals("set")) {
-			currentState.put(name, value);
+		if (command.equals("set")) {
+			/*
+			 * Set: push the command on the stack for easy rollback/commit, put
+			 * the addition into the cache, increment cached value count
+			 * 
+			 * Recall that set <name> <value> is actually 3 parts
+			 */
+			commands.push(new String[] { command, key, value });
+			currentState.put(key, value);
 			currentValueCount.put(value, ++valueCount);
 			return "";
 		}
-		if (name.equals("unset")) {
+		if (command.equals("unset")) {
+			/*
+			 * Unset: push command onto stack for easy rollback/commit
+			 * 
+			 * Add the deleted key/value pair to justDeleted if it exists in the
+			 * permanent DB. This takes care of the case where unset is called
+			 * on an unset value that exists in the permanent DB
+			 * 
+			 * Decrement the currentValueCount for the key/value pair if the
+			 * key/value pair existed in either the permanent or current DBs
+			 */
+			commands.push(new String[] { command, key });
 			/*
 			 * Only reduce a valueCount if the key/value to be removed exists in
 			 * the original DB or transaction's state
 			 */
-			if (currentState.remove(name) != null || Main.database.containsKey(name)) {
+			if (Main.database.containsKey(key)) {
+				justDeleted.add(key);
+			}
+			if (Main.database.containsKey(key) || currentState.containsKey(key)) {
 				currentValueCount.put(value, --valueCount);
+				currentState.remove(key);
 			}
 			return "";
 		}
-		if (name.equals("get")) {
-			if (currentState.containsKey(name)) {
-				return currentState.get(name);
+		if (command.equals("get")) {
+			if (currentState.containsKey(key) || justDeleted.contains(key)) {
+				/*
+				 * If the get value exists in current state, take it from there.
+				 * If it's in justDelete (meaning it exists in the main DB but
+				 * was unset in a transaction), then return null
+				 */
+				return currentState.get(key);
 			}
-			return Main.database.get(name);
+			return Main.database.get(key);
 		}
-		if (name.equals("numequalto")) {
+		if (command.equals("numequalto")) {
+			/*
+			 * Jsut return the sum of the counts in the permanent DB and current
+			 * transaction's state
+			 */
 			Integer current = currentValueCount.get(value);
 			Integer permanent = Main.valueCount.get(value);
 			if (current == null) {
@@ -56,15 +99,29 @@ public class Transaction {
 		return null;
 	}
 
-	public String[] pop() {
-		return commands.pop();
-	}
+	public void commit(Map<String, String> database) throws Exception {
+		/*
+		 * Because commands are stored in a stack (LIFO), once a key is actioned
+		 * upon we can ignore it for all future occurrences
+		 */
+		Set<String> ignoredKeys = new HashSet<>();
+		while (!commands.isEmpty()) {
+			String[] commandArray = commands.pop();
+			String key = commandArray[1];
 
-	public boolean isEmpty() {
-		return commands.isEmpty();
-	}
+			if (!ignoredKeys.contains(key)) {
+				String command = commandArray[0];
+				String value = commandArray[2];
 
-	public String getUpdatedValue(String name) {
-		return currentState.get(name);
+				if (command.equals("set")) {
+					database.put(key, value);
+				} else if (command.equals("unset")) {
+					database.remove(key);
+				} else {
+					throw new Exception("Unknown command in transaction command history: " + command + " " + key);
+				}
+				ignoredKeys.add(key);
+			}
+		}
 	}
 }
